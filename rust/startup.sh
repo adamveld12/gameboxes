2dfdd7622b3ef11a00353db52705e9b07d4e3e1b
Adam Veldhousen <adam@veldhousen.ninja>
2016-02-28 12:29:33 -0600

    figured out where rust loads configs from

--- rust/startup.sh
+++ rust/startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 
-if [[ -f ./cfg/server.cfg ]]; then
-  echo "Starting from server config";
-  ./RustDedicated -batchmode -load +server.seed $SEED;
+if [[ -f /configuration/server.cfg ]]; then
+  echo "Loading settings from server.cfg...";
+  mkdir -p /home/steam/game/server/rust_server/cfg;
+  ln -s /configuration/server.cfg /home/steam/game/server/rust_server/cfg/server.cfg;
+  ./RustDedicated -batchmode -load +server.identity "rust_server" -logfile "./logfile.txt";
   exit 0;
 fi
 
@@ -11,6 +13,7 @@ echo "$HOSTNAME starting...";
 ./RustDedicated -batchmode \
  +server.datadir /data \
  +server.hostname $HOSTNAME \
+ +server.identity "rust_server" \
  +server.seed $SEED \
  +server.serverlog 1 \
  +server.netlog 1 \
@@ -20,4 +23,5 @@ echo "$HOSTNAME starting...";
  +server.port 28015 \
  +rcon.ip 0.0.0.0 \
  +rcon.port 28016 \
- +rcon.password "${RCON_PASSWORD}";
+ +rcon.password "${RCON_PASSWORD}" \
+ -logfile "./logfile.txt";
