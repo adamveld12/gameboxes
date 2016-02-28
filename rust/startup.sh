81402ce9c7243702f68a2147eef26344a8c40482
Adam Veldhousen <adam@veldhousen.ninja>
2016-02-28 15:53:16 -0600

    woohoo

--- rust/startup.sh
+++ rust/startup.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
+set -euo pipefail;
 
+CONFIG_PATH="/home/steam/game/server/rust_server/cfg";
+LOG_PATH="/home/steam/game/logfile.txt";
 
 if [[ -f /configuration/server.cfg ]]; then
-  echo "Loading settings from server.cfg...";
-  mkdir -p ./server/rust_server/cfg/;
-  ln -s /configuration/server.cfg ./server/rust_server/cfg/server.cfg;
-  ./RustDedicated -batchmode -load +server.identity "rust_server" -logfile "./logfile.txt";
-  exit 0;
+  echo "Found a custom server.cfg, linking into ${CONFIG_PATH}...";
+  mkdir -p $CONFIG_PATH;
+  ln -sf /configuration/server.cfg $CONFIG_PATH/server.cfg;
 fi
 
-echo "$HOSTNAME starting...";
-./RustDedicated -batchmode \
- +server.datadir /data \
- +server.hostname $HOSTNAME \
- +server.identity "rust_server" \
- +server.seed $SEED \
- +server.serverlog 1 \
- +server.netlog 1 \
- +server.saveinternal 300 \
- +spawn.max_rate 1 \
- +server.maxplayers $MAX_PLAYERS \
- +server.port 28015 \
- +rcon.ip 0.0.0.0 \
- +rcon.port 28016 \
- +rcon.password "${RCON_PASSWORD}" \
- -logfile "./logfile.txt";
+./RustDedicated -batchmode -load +server.identity "rust_server" -logfile $LOG_PATH &
+
+sleep 2;
+tail -f $LOG_PATH;
+/bin/bash;
